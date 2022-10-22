92de57d9776d46e0d1b5fbb87f21a94534a132e5
Marty Trujillo <marty@ilikeemail.com>
2022-10-22 14:17:40 -0400

    adds dummy get data

---
+++ src/api/winners.php
@@ -0,0 +1,5 @@
+[
+    {name: 'Bubbu', amount: '100', picked: 'The big one'},
+    {name: 'Bibbu', amount: '100', picked: 'The little one'},
+    {name: 'Babbu', amount: '100', picked: 'The middle one'},
+]
